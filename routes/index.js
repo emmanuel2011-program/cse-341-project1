c0cd582538ef911138a162b86592a3c487485e5f
emmanuel2011 <okoyee61@gmail.com>
2025-05-16 16:53:49 +0100

    connectedmogodb, uploaded data, used get, getall

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-const router = require('express').Router();
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const express = require('express');
+const router = express.Router();
+
+router.use('/contacts', require('./contacts'))
+
 module.exports = router;
