92b8b85a11a176fd5ee7c94413a71412150e4189
emmanuel2011 <okoyee61@gmail.com>
2025-05-17 04:23:54 +0100

    working on error handling

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,16 +10,26 @@ const getAll = async (req, res, next) => {
 };
 
 const getSingle = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
+  try {
+    const userId = new ObjectId(req.params.id); // this will throw if ID is invalid
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .find({ _id: userId });
+    const lists = await result.toArray();
+
+    if (!lists.length) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
-  });
+  } catch (err) {
+    console.error('Error fetching single contact:', err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
+
 
 module.exports = { getAll, getSingle };
