915e67092f5087003a4b9424e3208c590772a1d0
emmanuel2011 <okoyee61@gmail.com>
2025-05-17 03:57:01 +0100

    added to check Api running

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+router.get('/', (req, res) => { 
+    res.send('API is running...');
+});
+
 
 router.use('/contacts', require('./contacts'))
 
