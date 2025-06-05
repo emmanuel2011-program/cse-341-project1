829be142c24877e261876cb8e4e642d765a9700d
emmanuel2011 <okoyee61@gmail.com>
2025-06-05 11:00:18 +0100

    edited

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Welcome to the API root!');
-});
-
 router.use('/swagger', require('./swagger'));
 router.use('/user', require('./user'));
 router.use('/theme', require('./theme'));
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app
