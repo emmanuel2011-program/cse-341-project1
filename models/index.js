f8b72f001505c4dda75279d6ea258bce537956be
emmanuel2011 <okoyee61@gmail.com>
2025-06-05 09:21:39 +0100

    added environment variable

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
-const dbConfig = require('../config/db.config.js');
-
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const db = {};
+// Use environment variable DB_URL if set, otherwise fallback to local MongoDB
+db.url = process.env.DB_URL || 'mongodb://localhost:27017/yourdbname';
+
 db.mongoose = mongoose;
-db.url = dbConfig.url;
 db.theme = require('./theme.js')(mongoose);
 db.user = require('./user.js')(mongoose);
 
